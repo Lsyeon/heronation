0076c90601575bc1f6e44d777d8948761dee0bb5
Lsyeon <baekgu1011@naver.com>
2020-01-14 14:25:48 +0900

    - mypageConnecting 회원정보 수정 버튼 활성화
    - userModifyActivity, user_modify.xml
    
    - userModify에서 아이디, 비밀번호 수정, 이름, 이메일, 생년월일 수정 가능
    
    - 수정:
    myPageConnectingActivity.java 수정
    userModifyActivity.java 추가
    drawerble: btn_background.xml 추가
    ic_access_time_black_24dp.xml 추가
    activity_my_page_connecting.xml 수정
    activity_user_modify.xml 추가
    AndroidManifest.xml 수정

--- README.md
+++
@@ -1,2 +0,0 @@
-# heronation
-android
--- app/src/main/java/com/example/heronation/userModifyActivity.java
+++ app/src/main/java/com/example/heronation/userModifyActivity.java
@@ -4,26 +4,38 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class myPageConnectingActivity extends AppCompatActivity {
+public class userModifyActivity extends AppCompatActivity {
     /* 상단 메뉴 버튼을 눌렀을 때 뜨는 레이아웃을 위한 변수들 */
     private DrawerLayout drawerLayout;
     private View drawerView;
 
+    /* 회원정보 수정에 있는 생일 선택을 위한 변수들*/
+    private TextView textView_Date;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         /* 상태표시바를 삭제해주는 작업 */
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_my_page_connecting);
+        setContentView(R.layout.activity_user_modify);
+
+        this.InitializeView();
+        this.InitializeListener();
 
         /* 상단바 메뉴 드로워 */
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -71,17 +83,19 @@ public class myPageConnectingActivity extends AppCompatActivity {
         public void onDrawerStateChanged(int newState) {
         }
     };
-    /*
+    /* 회원정보 edit버튼을 눌렀을때 다시 Mypage Activity 이동
      * Item 버튼을  눌렀을 때, Item Activity 이동
      * Shop 버튼을 눌렀을 때, Shop Activity 이동
      * 측정 버튼을 눌렀을 때, Measurement Activity 이동
      * 찜 버튼을 눌렀을 때, Wishlist Activity 이동
      * 마이 페이지 버튼을 눌렀을 때, Mypage Activity 이동
      */
-    public void Click_userModify_btn(View view){
-        Intent intent=new Intent(this, userModifyActivity.class);
+    public void Click_userModify_edit_btn(View view){
+        Intent intent=new Intent(this, myPageConnectingActivity.class);
         startActivity(intent);
+        Toast.makeText(this, "수정되었습니다.", Toast.LENGTH_LONG).show();
     }
+
     public void Click_itemButton(View view){
         Intent intent=new Intent(this, ItemActivity.class);
         startActivity(intent);
@@ -98,5 +112,35 @@ public class myPageConnectingActivity extends AppCompatActivity {
         Intent intent=new Intent(this,WishlistActivity.class);
         startActivity(intent);
     }
+    public void Click_mypageButton(View view){
+        Intent intent=new Intent(this,MypageActivity.class);
+        startActivity(intent);
+    }
 
+    /*날짜정보를 TextView에 표시하기 위해서 findViewByld()를 통해 화면에 표시되는 TextView의 참조객체를 얻어오는 함수*/
+    public void InitializeView()
+    {
+        textView_Date = (TextView)findViewById(R.id.textView_date);
+    }
+
+    /* OnDateSetListener를 구현하는 함수 */
+    public void InitializeListener()
+    {
+        callbackMethod = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                textView_Date.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth + "일");
+            }
+        };
+    }
+
+    /*이벤트 발생에 대해 처리를 담당*/
+    public void OnClickHandler(View view)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
+
+        dialog.show();
+    }
 }
